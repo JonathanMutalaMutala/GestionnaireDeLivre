64ed4ef55e2513553fee742d66c4af10b2e686d4
JonathanMutala <jonamut12.live@gmail.com>
2021-07-17 11:17:15 -0300

    Creation de la classe LivreModel.cs et creation de la base de la base de donnée LivreBD

---
+++ GestionLivre_JonathanMutala/Models/LivreModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionLivre_JonathanMutala.Models
+{
+    public class LivreModel
+    {
+        [Key]
+        public int IDLivre { get; set; }
+        [Required]
+        public string TitreLivre { get; set; }
+        [Required]
+        public string AuteurLivre { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DatePublicationLivre { get; set; }
+
+    }
+}
